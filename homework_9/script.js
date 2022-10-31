40d4fa97e3f2a442ee2914262b485d874f70f001
Roman Kartashov <kartashroma94@gmail.com>
2022-10-31 20:46:14 +0500

    add homework_9

---
+++ homework_9/script.js
@@ -0,0 +1,25 @@
+const fibonacci = function makeFibonacciFunction() {
+    let previousNumber = 0;
+    let nextNumber = 1;
+    
+
+    return function () {
+        let result = previousNumber + nextNumber;
+        console.log(previousNumber);
+        previousNumber = nextNumber;
+        nextNumber = result;
+    };
+ }();
+
+
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
